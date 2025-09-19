cb25ce8319d2fcd78604f138197848faf64a5e16
mohannadprogrammer <waheedmohannad55578@gmail.com>
2025-09-19 23:14:20 +0200

    [add] update route to post

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,16 +1,18 @@
 const models =require('../models');
+const { get } = require('../routes/posts');
 
 
 function save (req , res){
-   
+   console.log("post ======== >",req);
    // get data from request body
     const post ={
         title : req.body.title,
         content : req.body.content,
         imageUrl : req.body.imageUrl,
-        catogryId : req.body.catogryId,
+        catogryId : 1,
         userId : 1
     }
+    console.log("post ======== >",post);
 
     //deling with Database using ORM (sequelize)
     models.Post.create(post).then(result=>{
@@ -41,7 +43,54 @@ function show (request , response){
     })
 }
 
+function index (req , res){
+
+    models.Post.findAll().then(result=>{
+        res.status(200).json(result)
+    }).catch(err=>{
+        res.status(500).json({
+            message : "something went wrong",
+            error : err
+        })
+    })
+}
+
+function update (req , res){
+    const id = req.params.id
+
+    models.Post.update(req.body,{ where : {id:id } }).then(result=>{
+        res.status(200).json({
+            message : "post updated successfully",
+            post : result
+        })
+    }).catch(err=>{
+        res.status(500).json({
+            message : "something went wrong",
+            error : err
+        })
+    })
+}
+
+function destroy (req , res){
+    const id = req.params.id
+    
+    models.Post.destroy({ where : {id:id } }).then(result=>{
+        res.status(200).json({
+            message : "post deleted successfully",
+            post : result
+        })
+    }).catch(err=>{
+        res.status(500).json({
+            message : "something went wrong",
+            error : err
+        })
+    })
+}
+
 module.exports = {
     save:save ,
-    show:show
+    show:show , 
+    index:index,
+    update:update ,
+    destroy:destroy
 }
--- routes/posts.js
+++ routes/posts.js
@@ -7,7 +7,14 @@ const router = express.Router();
 // create post route 
 router.post('/',postsController.save)
 
+// get all posts route
+router.get('/',postsController.index)
+
 // get post by id route
 router.get('/:id',postsController.show)
+
+router.put('/:id',postsController.update)
+
+router.delete('/:id',postsController.destroy)    
 
 module.exports =router
